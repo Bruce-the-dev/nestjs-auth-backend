f38c194caa318da9cdb7a60a1de72a91754ee30c
Bruce-the-dev <mutsinzibruce@gmail.com>
2025-03-05 19:44:41 +0200

    enabled cors origin for flutter web

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Enable CORS to allow requests from Flutter Web
+  app.enableCors({
+    origin: '*', // Allow all origins (use specific origins in production)
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
